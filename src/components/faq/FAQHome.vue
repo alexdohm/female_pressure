2c5ac289547644378471d78320911105a746735c
electricin <85707782+electricin@users.noreply.github.com>
2021-08-09 19:44:34 +0200

    reinstating the old, unstructured faq accordion until i have programmed the section headers

--- src/components/faq/FAQHome.vue
+++ src/components/faq/FAQHome.vue
@@ -3,182 +3,40 @@
     <Header/>
     <b-container class="mt-5 body-container">
 
+      <!-- FIRST ATTEMPT TO ADD h3 SUB TITLES
       <div class="mb-4">
         <h3>membership & database</h3>
       </div>
+     answers 1: membership & database
+     answers 2: mailing list
+     answers 3: login & public artist profile
+     answers 4: participation in female:pressure projects
+     answers 5: female:pressure community
+     answers 6: similar projects & third party involvement
+      -->
+
       <div class="accordion" role="tablist">
         <b-card
             no-body
             class="mb-1 main-card"
             v-for="(question, idx) in faqData"
-            :key="question.title1"
+            :key="question.title"
         >
           <b-card-header
               class="accordion-container mb-3 accordion-button h5 py-1 px-2"
               role="tab"
               v-b-toggle="'accordion-' + idx"
           >
-            <b class="question-title">{{ question.title1 }}</b>
+            <b class="question-title">{{ question.title }}</b>
           </b-card-header>
 
-          <!-- answers 1: membership & database -->
           <b-collapse
               :id="'accordion-' + idx"
               accordion="my-accordion"
               role="tabpanel"
               class="mb-3"
           >
-            <p v-html="question.answer1" class="m-1"></p>
-          </b-collapse>
-        </b-card>
-      </div>
-
-      <div class="mb-4">
-        <h3>mailing list</h3>
-      </div>
-      <div class="accordion" role="tablist">
-        <b-card
-            no-body
-            class="mb-1 main-card"
-            v-for="(question, idx) in faqData"
-            :key="question.title2"
-        >
-          <b-card-header
-              class="accordion-container mb-3 accordion-button h5 py-1 px-2"
-              role="tab"
-              v-b-toggle="'accordion-' + idx"
-          >
-            <b class="question-title">{{ question.title2 }}</b>
-          </b-card-header>
-
-          <!-- answers 2: mailing list -->
-          <b-collapse
-              :id="'accordion-' + idx"
-              accordion="my-accordion"
-              role="tabpanel"
-              class="mb-3"
-          >
-            <p v-html="question.answer2" class="m-1"></p>
-          </b-collapse>
-        </b-card>
-      </div>
-
-      <div class="mb-4">
-        <h3>login & public artist profile</h3>
-      </div>
-      <div class="accordion" role="tablist">
-        <b-card
-            no-body
-            class="mb-1 main-card"
-            v-for="(question, idx) in faqData"
-            :key="question.title3"
-        >
-          <b-card-header
-              class="accordion-container mb-3 accordion-button h5 py-1 px-2"
-              role="tab"
-              v-b-toggle="'accordion-' + idx"
-          >
-            <b class="question-title">{{ question.title3 }}</b>
-          </b-card-header>
-
-          <!-- answers 3: login & public artist profile -->
-          <b-collapse
-              :id="'accordion-' + idx"
-              accordion="my-accordion"
-              role="tabpanel"
-              class="mb-3"
-          >
-            <p v-html="question.answer3" class="m-1"></p>
-          </b-collapse>
-        </b-card>
-      </div>
-
-      <div class="mb-4">
-        <h3>participation in female:pressure projects</h3>
-      </div>
-      <div class="accordion" role="tablist">
-        <b-card
-            no-body
-            class="mb-1 main-card"
-            v-for="(question, idx) in faqData"
-            :key="question.title4"
-        >
-          <b-card-header
-              class="accordion-container mb-3 accordion-button h5 py-1 px-2"
-              role="tab"
-              v-b-toggle="'accordion-' + idx"
-          >
-            <b class="question-title">{{ question.title4 }}</b>
-          </b-card-header>
-
-          <!-- answers 4: participation in female:pressure projects -->
-          <b-collapse
-              :id="'accordion-' + idx"
-              accordion="my-accordion"
-              role="tabpanel"
-              class="mb-3"
-          >
-            <p v-html="question.answer4" class="m-1"></p>
-          </b-collapse>
-        </b-card>
-      </div>
-
-      <div class="mb-4">
-        <h3>female:pressure community</h3>
-      </div>
-      <div class="accordion" role="tablist">
-        <b-card
-            no-body
-            class="mb-1 main-card"
-            v-for="(question, idx) in faqData"
-            :key="question.title5"
-        >
-          <b-card-header
-              class="accordion-container mb-3 accordion-button h5 py-1 px-2"
-              role="tab"
-              v-b-toggle="'accordion-' + idx"
-          >
-            <b class="question-title">{{ question.title5 }}</b>
-          </b-card-header>
-
-          <!-- answers 5: female:pressure community -->
-          <b-collapse
-              :id="'accordion-' + idx"
-              accordion="my-accordion"
-              role="tabpanel"
-              class="mb-3"
-          >
-            <p v-html="question.answer5" class="m-1"></p>
-          </b-collapse>
-        </b-card>
-      </div>
-
-      <div class="mb-4">
-        <h3>similar projects & third party involvement</h3>
-      </div>
-      <div class="accordion" role="tablist">
-        <b-card
-            no-body
-            class="mb-1 main-card"
-            v-for="(question, idx) in faqData"
-            :key="question.title6"
-        >
-          <b-card-header
-              class="accordion-container mb-3 accordion-button h5 py-1 px-2"
-              role="tab"
-              v-b-toggle="'accordion-' + idx"
-          >
-            <b class="question-title">{{ question.title6 }}</b>
-          </b-card-header>
-
-          <!-- answers 6: similar projects & third party involvement -->
-          <b-collapse
-              :id="'accordion-' + idx"
-              accordion="my-accordion"
-              role="tabpanel"
-              class="mb-3"
-          >
-            <p v-html="question.answer6" class="m-1"></p>
+            <p v-html="question.answer" class="m-1"></p>
           </b-collapse>
         </b-card>
       </div>
