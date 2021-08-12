f003c44378cd55fd68c16f843183b92ecf9feaef
Alex Dohm <dohma@oregonstate.edu>
2021-08-12 20:54:01 +0200

    faq guide

--- src/components/faq/FAQHome.vue
+++ src/components/faq/FAQHome.vue
@@ -1,7 +1,42 @@
 <template>
   <div>
     <Header/>
-    <b-container class="mt-5 body-container">
+    <b-container class="mt-5 f-body-container">
+
+      <div v-for="(topic, index) in faqData"
+           :key="'topic' + index"
+           >
+        <h3>{{topic.title}}</h3>
+        <div class="accordion" role="tablist">
+          <b-card
+              no-body
+              class="mb-1 main-card"
+              v-for="(question, index2) in topic.questions"
+              :key="'question' + index + index2"
+          >
+            <b-card-header
+                class="accordion-container mb-3 accordion-button h5 py-1 px-2"
+                role="tab"
+                v-b-toggle="'accordion-' + index + index2"
+            >
+              <b class="question-title">{{ question.title }}</b>
+            </b-card-header>
+
+            <!-- answers 1: membership & database -->
+            <b-collapse
+                :id="'accordion-' + index + index2"
+                accordion="my-accordion"
+                role="tabpanel"
+                class="mb-3"
+            >
+              <p v-html="question.answer" class="m-1"></p>
+            </b-collapse>
+          </b-card>
+        </div>
+      </div>
+
+      /////
+
 
       <div class="mb-4">
         <h3>membership & database</h3>
