38ee6373d52513c7f688c697d8dac5d91ecd595e
inesfcn <67946487+inesfcn@users.noreply.github.com>
2021-09-12 14:22:27 +0200

    created new component merchmodal

--- src/components/merch/MerchHome.vue
+++ src/components/merch/MerchHome.vue
@@ -44,62 +44,7 @@
         </h5>
 
         <button class="f-button mt-4" @click="showModal = true">order</button>
-
-        <div v-if="showModal" class="modal-mask">
-          <div class="modal-wrapper">
-            <div class="modal-container d-flex flex-column">
-              <button
-                @click="showModal = false"
-                class="close-button align-self-end"
-              >
-                X
-              </button>
-              <h5>Order proccess: </h5>
-              <p>
-                We are only abble to proccess orders with advance payment and we
-                will need a proof of payment!
-              </p>
-
-              <table>
-                <tr>
-                  <th>size chart</th>
-                  <th>chest [cm]</th>
-                  <th>length [cm]</th>
-                </tr>
-                <tr>
-                  <td>XS</td>
-                  <td>98</td>
-                  <td>68</td>
-                </tr>
-                <tr>
-                  <td>S</td>
-                  <td>102</td>
-                  <td>70.5</td>
-                </tr>
-                <tr>
-                  <td>M</td>
-                  <td>108</td>
-                  <td>73</td>
-                </tr>
-                <tr>
-                  <td>L</td>
-                  <td>114</td>
-                  <td>75.5</td>
-                </tr>
-                <tr>
-                  <td>XL</td>
-                  <td>120</td>
-                  <td>78</td>
-                </tr>
-                <tr>
-                  <td>XXL</td>
-                  <td>126</td>
-                  <td>80.5</td>
-                </tr>
-              </table>
-            </div>
-          </div>
-        </div>
+        <MerchModal :showModal="showModal" />
       </div>
 
       <!-- ARCHIVE -->
@@ -161,6 +106,7 @@
 <script>
 import Header from '@/components/header/Header';
 import { mapMutations } from 'vuex';
+import MerchModal from './MerchModal.vue';
 
 export default {
   data() {
@@ -171,6 +117,7 @@ export default {
 
   components: {
     Header,
+    MerchModal,
   },
   methods: {
     ...mapMutations(['SET_PAGE']),
---
+++ src/components/merch/MerchModal.vue
@@ -0,0 +1,62 @@
+<template>
+  <div v-if="showModal" class="modal-mask">
+    <div class="modal-wrapper">
+      <div class="modal-container d-flex flex-column">
+        <button @click="showModal = false" class="close-button align-self-end">
+          X
+        </button>
+        <h5>Order proccess:</h5>
+        <p>
+          We are only abble to proccess orders with advance payment and we will
+          need a proof of payment!
+        </p>
+
+        <table>
+          <tr>
+            <th>size chart</th>
+            <th>chest [cm]</th>
+            <th>length [cm]</th>
+          </tr>
+          <tr>
+            <td>XS</td>
+            <td>98</td>
+            <td>68</td>
+          </tr>
+          <tr>
+            <td>S</td>
+            <td>102</td>
+            <td>70.5</td>
+          </tr>
+          <tr>
+            <td>M</td>
+            <td>108</td>
+            <td>73</td>
+          </tr>
+          <tr>
+            <td>L</td>
+            <td>114</td>
+            <td>75.5</td>
+          </tr>
+          <tr>
+            <td>XL</td>
+            <td>120</td>
+            <td>78</td>
+          </tr>
+          <tr>
+            <td>XXL</td>
+            <td>126</td>
+            <td>80.5</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    showModal: Boolean,
+  },
+};
+</script>
